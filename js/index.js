407c3ceaef8c6bf61a606b0f07408e4db433c3d2
J!N <saga.gin.jin@gmail.com>
2019-05-17 12:40:24 +1200

    size selection, show and hide my cart

--- js/index.js
+++ js/index.js
@@ -1,6 +1,21 @@
+//size selection when click the size button
 $( ".size-selector" ).click(function() {
-    $( ".size-selector" ).removeClass( 'size-selected' );
-    $( this ).addClass("size-selected");
-    var size = $( this ).html();
-    $( ".selected-size" ).text(size);
+    if ($( ".size-selector" ).hasClass( "size-selected" )) {
+        $( ".size-selector" ).removeClass( 'size-selected' );
+        $( ".selected-size" ).text("");
+    } else {
+        $( ".size-selector" ).removeClass( 'size-selected' );
+        $( this ).addClass("size-selected");
+        var size = $( this ).html();
+        $( ".selected-size" ).text(size);
+    }
+});
+
+//show and hide may cart when click the my cart button
+$( ".my-cart" ).click(function() {
+    if ($( ".minicart" ).hasClass( "show" )) {
+        $( ".minicart" ).removeClass( 'show' );
+    } else {
+        $( ".minicart" ).addClass( 'show' );
+    }
 });
